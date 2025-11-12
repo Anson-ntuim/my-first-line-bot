f9a355c4757d52ae83ff5922be0ba2226996c88d
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 13:33:21 +0800

    Update api/line.js to use Bottender standard deployment for Vercel

--- api/line.js
+++ api/line.js
@@ -1,26 +1,10 @@
-const express = require('express');
-const { createServer } = require('@bottender/express');
-const App = require('../src');
+const { bottender } = require('bottender');
 
-// 建立 Express app
-const app = express();
-
-// 重要：在 body parser 之前，保留原始 body 供簽名驗證使用
-app.use(express.json({
-  verify: (req, res, buf) => {
-    // 將原始 body 存到 req.rawBody，供 Bottender 驗證簽名使用
-    req.rawBody = buf.toString('utf8');
-  }
-}));
-
-// 使用 @bottender/express 建立 Bottender server
-const bottenderServer = createServer(App, {
+// 使用 bottender() 建立 bot，它會自動載入 src/index.js
+const bot = bottender({
   dev: false,
 });
 
-// 將 Bottender server 掛載到 Express app
-app.use(bottenderServer);
-
-// 導出 Express app（Vercel 可以直接使用）
-module.exports = app;
+// bottender() 返回的是一個 Express app，可以直接導出
+module.exports = bot;
 
