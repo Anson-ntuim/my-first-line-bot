04be9a6a5e48e602188d68d0534f530813a6cb16
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 11:37:17 +0800

    editing path

--- bottender.config.js
+++ bottender.config.js
@@ -40,8 +40,9 @@ module.exports = {
     },
     line: {
       enabled: true,
-      path: '/webhooks/line',
-      accessToken: process.env.LINE_ACCESS_TOKEN,
+      path: process.env.LINE_WEBHOOK_PATH || '/api/line',
+      accessToken:
+        process.env.LINE_ACCESS_TOKEN || process.env.LINE_CHANNEL_ACCESS_TOKEN,
       channelSecret: process.env.LINE_CHANNEL_SECRET,
     },
     telegram: {
