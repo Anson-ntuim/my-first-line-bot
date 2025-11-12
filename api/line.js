ab42beca3cdad2c8c1e0e39fdaed8ed5be907889
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 11:53:14 +0800

    debug

--- api/line.js
+++ api/line.js
@@ -1,14 +1,5 @@
 const { bottender } = require('bottender');
 
-const handler = bottender({
+module.exports = bottender({
   dev: false,
 });
-
-module.exports = async (req, res) => {
-  try {
-    await handler(req, res);
-  } catch (error) {
-    console.error('Error handling request:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
