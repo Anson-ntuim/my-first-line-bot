1f443d711a04c4e00af736bc3aa287f413de3011
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 13:35:40 +0800

    Fix: Wrap bottender as Vercel serverless function

--- api/line.js
+++ api/line.js
@@ -5,6 +5,21 @@ const bot = bottender({
   dev: false,
 });
 
-// bottender() 返回的是一個 Express app，可以直接導出
-module.exports = bot;
+// 包裝成 Vercel serverless function
+module.exports = async (req, res) => {
+  // bottender 會處理所有的請求
+  if (bot && typeof bot === 'function') {
+    return bot(req, res);
+  } else if (bot && bot.requestHandler) {
+    return bot.requestHandler(req, res);
+  } else {
+    // 如果 bot 有 listen 方法，表示它是 Express app
+    // 需要手動處理請求
+    const handler = bot._router || bot;
+    if (typeof handler === 'function') {
+      return handler(req, res);
+    }
+    res.status(500).json({ error: 'Invalid bot configuration' });
+  }
+};
 
