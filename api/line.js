9939c888430bada0422b632e3ea84da8083f300e
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 13:29:31 +0800

    console

--- api/line.js
+++ api/line.js
@@ -1,11 +1,26 @@
-const { bottender } = require('bottender');
+const express = require('express');
+const { createServer } = require('@bottender/express');
+const App = require('../src');
 
-const app = bottender({
+// 建立 Express app
+const app = express();
+
+// 重要：在 body parser 之前，保留原始 body 供簽名驗證使用
+app.use(express.json({
+  verify: (req, res, buf) => {
+    // 將原始 body 存到 req.rawBody，供 Bottender 驗證簽名使用
+    req.rawBody = buf.toString('utf8');
+  }
+}));
+
+// 使用 @bottender/express 建立 Bottender server
+const bottenderServer = createServer(App, {
   dev: false,
 });
 
-const handler = app.getRequestHandler();
+// 將 Bottender server 掛載到 Express app
+app.use(bottenderServer);
 
-module.exports = async (req, res) => {
-  return handler(req, res);
-};
+// 導出 Express app（Vercel 可以直接使用）
+module.exports = app;
+
