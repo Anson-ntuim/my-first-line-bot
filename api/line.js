6395e00d5ce60dcb1d14fd6ae4737956b39fbbf0
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 11:57:02 +0800

    debug

--- api/line.js
+++ api/line.js
@@ -1,5 +1,10 @@
 const { bottender } = require('bottender');
 
-module.exports = bottender({
+const app = bottender({
   dev: false,
 });
+
+// Vercel serverless function 需要一個函數
+module.exports = (req, res) => {
+  return app(req, res);
+};
