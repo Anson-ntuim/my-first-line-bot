34da99cfa4c5a88fa9eac564dfc01c7f3b219dee
Anson-ntuim <AnsonT320@gmail.com>
2025-11-12 15:02:19 +0800

    132

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const { router, text, route, payload } = require('bottender/router');
 
+let money = [0, 0, 0, 0];
+let currentIndex = 0;
+const categories = ['餐飲', '交通', '娛樂', '其他'];
+
+
 async function handleText(context) {
   const userText = context.event.text;
   await context.sendText(`你說了:${userText}`);
+}
+
+async function handleMoney(context) {
+  money[currentIndex] = Number(context.event.text) + money[currentIndex];
+  await context.sendText(`${context.event.text} 已記錄，目前${categories[currentIndex]}總為: ${money[currentIndex]}`);
 }
 
 async function handleSticker(context) {
@@ -22,23 +32,40 @@ async function handleFollow(context) {
   await context.sendText('感謝你加我為好友!');
 }
 
+async function handleCategory(context) {
+  if (context.event.text === '餐飲') {
+    currentIndex = 0;
+  } else if (context.event.text === '交通') {
+    currentIndex = 1;
+  } else if (context.event.text === '娛樂') {
+    currentIndex = 2;
+  } else if (context.event.text === '其他') {
+    currentIndex = 3;
+  }
+  await context.sendText(`你選擇了:${context.event.text}`);
+  await context.sendText(`請輸入金額:`);
+}
+
 async function showMenu(context) {
   await context.sendButtonTemplate('選單', {
-    text: '請選擇功能:',
+    text: '請選擇花費:',
     actions: [
-      { type: 'message', label: '功能1', text: '功能1' },
-      { type: 'message', label: '功能2', text: '功能2' },
-      { type: 'postback', label: '功能3', data: 'action=功能3' }
+      { type: 'message', label: '餐飲', text: '餐飲' },
+      { type: 'message', label: '交通', text: '交通' },
+      { type: 'message', label: '娛樂', text: '娛樂' },
+      { type: 'message', label: '其他', text: '其他' },
     ]
   });
 }
 
 module.exports = function App() {
   return router([
-    text('選單|menu', showMenu),
+    text(/^記帳$/i, showMenu),
     text(/^hello|hi|你好$/i, async (context) => {
       await context.sendText('你好!');
     }),
+    text(/^\d+$/i, handleMoney),
+    text(/^餐飲|交通|娛樂|其他$/i, handleCategory),
     text('*', handleText),
     route(context => context.event.isSticker, handleSticker),
     route(context => context.event.isImage, handleImage),
